9f7df70f1ace7d8fe6ff18a97ff98b8ed39145cf
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-08-20 16:59:22 -0300

    first commit

---
+++ src/main/java/com/einando/edumanager/EdumanagerApplication.java
@@ -0,0 +1,13 @@
+package com.einando.edumanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdumanagerApplication.class, args);
+	}
+
+}
